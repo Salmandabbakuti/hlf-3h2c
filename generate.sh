64ca2915a4f88f0dcb9f7cc4a3e43ca1b8fe904a
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-16 18:38:52 +0530

    Create generate.sh

---
+++ generate.sh
@@ -0,0 +1,14 @@
+echo 'generating Certificates..'
+rm -rf crypto-config
+rm -rf channel-artifacts
+mkdir channel-artifacts
+export PATH=$PWD/bin:$PATH
+export FABRIC_CFG_PATH=$PWD
+cryptogen generate —-config=./crypto-config.yaml
+echo 'Generating genesis block and channel configurations..'
+configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+
+configtxgen -profile ChannelAll -outputCreateChannelTx ./channel-artifacts/channelall.tx -channelID channelall
+configtxgen -profile Channel12 -outputCreateChannelTx ./channel-artifacts/channel12.tx -channelID channel12
+
+exit 1
